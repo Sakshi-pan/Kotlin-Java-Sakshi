570a0f2b91fed0d15f6aebf4ee29c9148bd3ffe9
Sakshi-pan <154324139+Sakshi-pan@users.noreply.github.com>
2024-01-17 18:42:44 +0530

    Add files via upload

---
+++ StudentDAOImple.java
@@ -0,0 +1,58 @@
+package com.spring.jpa.PostgreDemo.PostgreDemo.DAO;
+
+import com.spring.jpa.PostgreDemo.PostgreDemo.Entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public class StudentDAOImple implements StudentDAO {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public StudentDAOImple(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student order by lastName", Student.class);
+        return theQuery.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        Student theStudent = entityManager.find(Student.class, id);
+        entityManager.remove(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+        int numRowDeleted = entityManager.createQuery("DELETE FROM Student").executeUpdate();
+        return numRowDeleted;
+    }
+}
